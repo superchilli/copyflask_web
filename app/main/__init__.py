252152f000a3a87e0f6d1a8656885f06e396a908
rose <51940320@qq.com>
2017-08-14 00:48:50 +0800

    update

---
+++ app/main/__init__.py
@@ -0,0 +1,11 @@
+from flash import Blueprint
+
+main = Blueprint('main', __name__)
+
+from . import views, errors
+from ..models import Permission
+
+
+@main.app_context_processor
+def inject_permissions():
+    return dict(Permission=Permission)
---
+++ app/main/views.py
@@ -0,0 +1,105 @@
+from flask import render_template, redirect, url_for, abort, flash, request,\
+    current_app
+from flask_login import login_required, current_user
+from . import main
+from .forms import EditProfileForm, EidtProfileAdminForm, PostForm
+from .. import db
+from ..models import Permission, Role, User, Post
+from ..decorators import admin_required
+
+
+@main.route('/', methods=['GET', 'POST'])
+def index():
+    form = PostForm()
+    if current_user.can(Permission.WRITE_ARTICLES) and \
+            form.validate_on_submit():
+        post = Post(body=form.body.data,
+                    author=current_user._get_current_object())
+        db.session.add(post)
+        return redirect(url_for('.index'))
+    page = request.args.get('page', 1, type=int)
+    pagination = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page, per_page=current_app.config['FLASKY_POSTS_PER_PAGE'],
+        error_out=False)
+    posts = pagination.items
+    return render_template('index.html', form=from, posts=posts,
+                            pagination=pagination)
+
+
+@main.route('/user/<username>')
+def user(username):
+    user=User.query.filter_by(username=username).first_or_404()
+    page=request.args.get('page', 1, type=int)
+    pagination = user.posts.order_by(Post.timestamp.desc()).paginate(
+        page, per_page=current_app.config['FLASKY_POSTS_PER_PAGE'],
+        error_out=False)
+    posts=pagination.items
+    return render_template('user.html', user=user, posts=posts,
+                           pagination=pagination)
+
+
+@main.route('/eidt-profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EidtProfileForm()
+    if form.validate_on_submit():
+        current_user.name=form.name.data
+        current_user.location=form.location.data
+        current_user.about_me=form.about_me.data
+        db.session.add(current_user)
+        flash('Your profile has been updated.')
+        return redirect(url_for('.user', username=current_user.username))
+    form.name.data=current_user.name
+    form.location.data=current_user.location
+    form.about_me.data=current_user.about_me
+    return render_template('edit_profile.heml', form=form)
+
+
+@main.route('/edit-profile/<int:id>', methods=['GET', 'POST'])
+@login_required
+@admin_required
+def edit_profile_admin(id):
+    user= User.query.get_or_404(id)
+    form = EditProfileAdminForm(user=user)
+    if form.validate_on_submit():
+        user.email = form.email.data
+        user.username = form.username.data
+        user.confirmed = form.confirmed.data
+        user.role=Role.query.get(form.role.data)
+        user.name = form.name.data
+        user.location=form.location.data
+        user.about_me=form.about_me.data
+        db.session.add(user)
+        flash('The profile has been updated.')
+        return redirect(url_for('.user', username=user.username))
+    form.email.data=user.email
+    form.username.data=user.username
+    form.confirmed.data=user.confirmed
+    form.role.data=user.role_id
+    form.name.data=user.name
+    form.location.data=user.location
+    form.about_me.data=user.about_me
+    return render_template('edit_profile.html', form=form, user=user)
+
+
+@main.route('/post/<int:id>')
+def post(id):
+    post = Post.query.get_or_404(id)
+    return render_template('post.html', posts=[post])
+
+
+@main.route('/edit/<int:id>', methods=['GET', 'POST'])
+@login_required
+def edit(id):
+    post=Post.query.get_or_404(id)
+    if current_user != post.author and \
+            not current_user.can(Permission.ADMINISTER):
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.body = form.body.data
+        db.session.add(post)
+        flash('The post has been updated.')
+        return redirect(url_for('.post', id=post.id))
+    form.body.data = post.body
+    return render_template('edit_post.html', form=form)
