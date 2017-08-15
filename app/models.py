1a30954c1dea0b12730e4ba65f42816539609a3f
rose <51940320@qq.com>
2017-08-15 15:32:59 +0800

    can choose to show all user's post or followed user's posts

--- app/models.py
+++ app/models.py
@@ -107,6 +107,14 @@ class User(UserMixin, db.Model):
             except IntegrityError:
                 db.session.rollback()
 
+    @staticmethod
+    def add_self_follows():
+        for user in User.query.all():
+            if not user.is_following(user):
+                user.follow(user)
+                db.session.add(user)
+                db.session.commit()
+
     def __init__(self, **kwargs):
         super(User, self).__init__(**kwargs)
         if self.role is None:
@@ -117,6 +125,7 @@ class User(UserMixin, db.Model):
         if self.email is not None and self.avatar_hash is None:
             self.avatar_hash = hashlib.md5(
                 self.email.encode('utf-8')).hexdigest()
+        self.followed.append(Follow(followed=self))
 
     @property
     def password(self):
@@ -222,6 +231,12 @@ class User(UserMixin, db.Model):
     def is_followed_by(self, user):
         return self.followers.filter_by(
             follower_id=user.id).first() is not None
+
+    @property
+    def followed_posts(self):
+        return Post.query.join(Follow, Follow.followed_id == Post.auther_id)\
+                .filter_by(Follow.follower_id == self.id)
+
 
     def __repr__(self):
         return '<User %r>' % self.username
